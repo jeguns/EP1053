46d149929b7d248255997e1368af221fdce8da57
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2020-07-29 17:09:17 -0500

    Actualización 2020.07.29

---
+++ Codigo 02.01 - Aprendiendo R.R
@@ -0,0 +1,19 @@
+812+65
+32-6
+15+6666985
+50%/%7 # El operador %/% sirve para obtener la parte entera
+50%%7  # El operador %% sirve para obtener el residuo
+50/7   # El operador / sirve para dividir dos números
+4^3
+4**3
+exp(3)
+exp(1)
+exp(6.25)
+sqrt(34)
+8**(1/3)
+log2(16)
+(56+6)*2%%sqrt(28) # colocar **2 en vez de *2
+(56+6)^2%%(28)^1/2 # colocar (1/2) en vez de 1/2
+(56+6)**2%/%sqrt(28) # colocar %% en vez de %/%
+(56+6)**2%%sqrt(28) # correct o
+300 %/% (450^(1/3))
