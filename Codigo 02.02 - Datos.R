9a25c502fea3621a9291c6e408ed423fe1ffd4cf
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2021-01-27 14:55:14 -0500

    Actualización 27/01

--- Codigo 02.02 - Datos.R
+++ Codigo 02.02 - Datos.R
@@ -59,7 +59,8 @@ is.double(a)
 is.complex(a)
 is.logical(a)
 
-(R = 3 == 5.2) # ¿3=5.2?
+(3 == 5.2) # ¿3=5.2?
+(R = 3 == 5.2) # ¿3=5.2? y lo almacena en el objeto R
 4.6 <= 8.1
 12 > 3.5
 
@@ -75,6 +76,7 @@ is.character(p)
 q = '70096321'
 is.double(q)
 is.character(q)
+s = 'En este taller se hará uso del paquete ggplot2 para visualizar gráficamente el comportamiento de variables relacionadas al COVID-19 en nuestro país, a partir de datos extraídos de la Plataforma Nacional de Datos Abiertos y la Sala Situacional del Ministerio de Salud.'
 
 # COERCIÓN DE DATOS
 
@@ -97,26 +99,16 @@ as.integer(24.12)
 
 # VECTOR
 
-(x1 = c(3, 4, 5, 12, 3.4))
+x1 = c(3,4,5,12,3.4)
 typeof(x1)
 str(x1)
 is.double(x1)
-is.logical(x1)
 is.vector(x1)
-is.list(x1)
-x1[1] 
-
-x1[c(1,2,5)] # Manera más general
-x1[c(1,5,2)]
-
-sort(x1) # de menor a mayor
-sort(x1, decreasing = TRUE) # de mayor a menor
-
-x1[1:4] # Similar a Excel A1:B30
+x1[1]
+x1[1:3]
 x1[c(1:3)]
 x1[-2]
 x1[-c(1:3)]
-x1[-(1:3)]
 
 y1 = c(FALSE, 5L, 6.3, 'abc123')
 typeof(y1)
@@ -124,52 +116,48 @@ str(y1)
 is.double(y1)
 is.vector(y1)
 y1[3]
-y1[1]
-y1[[1]]
+
+sort(x1) # de menor a mayor
+sort(x1, decreasing = TRUE) # de mayor a menor
 
 # LISTA
-# Toda lista es un vector
-# No todo vector es una lista
+# Toda lista puede tener características de un vector
+# Todo vector no necesariamente tiene las características una lista
 
 x2 = list(1L, 'a')
 typeof(x2)
 str(x2)
 is.vector(x2)
 is.list(x2)
-x2[2] #crea una sublista con la segunda posición
-x2[[2]] #extrae el elemento de la posición 2
-str(x2[2])
-str(x2[[2]])
+x2[[2]]
+is.double(x2)
+is.integer(x2)
+is.character(x2)
 
-y2 = list(m = c(3,4,5), n = c(7,24,25), p = matrix((c(2,2,2,2)),ncol=2))
+x3 = list(1L, 2L)
+is.integer(x3)
+
+y2 = list(m = c(3,4,5), n = c(7,24,25))
 typeof(y2)
 str(y2)
 is.vector(y2)
 is.list(y2)
 y2[[2]]
-y2$n
 y2[[2]][3]
 y2$n[3]
-y2$m
-y2$m[2]
 
 # MATRIZ
 
-x3 = matrix(c(7,3,5,8),ncol=2) # almacena una matriz con 2 columnas
-x3 = matrix(c(7,3,5,8),nrow=2,byrow=TRUE)
-x3 = matrix(c(7,3,5,8),nrow=2,bycol=TRUE) # no existe bycol
+x3 = matrix(c(7,3,5,8),ncol=2)
 typeof(x3)
 str(x3)
 is.double(x3)
 is.vector(x3)
 is.list(x3)
 is.matrix(x3)
-x3[1,2] # matriz[fila,columna]
-x3[2,]  # extrae segunda fila
-x3[,2]  # extrae segunda columna
-
-x_3 = matrix(1:99,ncol=9)
-x_3[5:7,4:8]
+x3[1,2]
+x3[2,]
+x3[,2]
 
 y3 = matrix(c(7,TRUE,5,8,1,2),nrow=3)
 typeof(y3)
@@ -182,15 +170,10 @@ y3[3,1]
 y3[2,]
 y3[,2]
 
-z3 = matrix(c(1,4,as.character(5),8),ncol=2) # hacemos coerción explícita y R hace implícita
 
 # DATA FRAME 
 
-x4 = data.frame(var1 = c(4,5,3,5,9), var2= c('1','3','66','12','15'), var3 = 4, var4 = "abc")
-x4 = data.frame(FALSE = c(4,5,3,5,9), var2= c('1','3','66','12','15'), var3 = 4, var4 = "abc") 
-# FALSE es una palabra reservada
-x4 = data.frame(False = c(4,5,3,5,9), var2= c('1','3','66','12','15'), var3 = 4, var4 = "abc") 
-
+x4 = data.frame(var1 = c(4,5,3,5,3), var2= c('1','3','66','12','15'))
 typeof(x4)
 str(x4)
 is.double(x4)
@@ -201,26 +184,20 @@ is.data.frame(x4)
 x4[1,2]
 x4[2,]
 x4[,2]
-x4$var3
+x4$var1
 x4$var2[3]
-attach(x4) # independiza cada columna
 
 # ARRAY o ARREGLO
 
-x5 = array(c(1,3,5,7,9,11,2,4,6,8,10,12), c(2, 3, 2))
-typeof(x5)
-str(x5)
-is.double(x5)
-is.vector(x5)
-is.list(x5)
-is.matrix(x5)
-is.data.frame(x5)
-is.array(x5)
+(A1 = c(1,3,4,10,10,20,2,1));str(A1)
+(B1 = array(c(1,3,4,10,10,20,2,1), dim = c(8))); str(B1)
+(C1 = array(c(1,3,4,10,10,20,2,1), dim = c(1,8))); str(B1)
+(D1 = array(c(1,3,4,10,10,20,2,1), dim = c(8,1))); str(C1)
+(E1 = array(c(1,3,4,10,10,20,2,1), dim = c(2,4))); str(D1)
+(F1 = array(c(1,3,4,10,10,20,2,1), dim = c(2,2,2)));str(E1)
+(G1 = array(matrix(c(1,3,4,10,10,20,2,1)),dim=c(2,2,2)));str(F1)
+(H1 = array(matrix(c(1,3,4,10,10,20,2,'1'),ncol=2),dim=c(2,2,2)));str(G1)
 x5[,,2]
 is.matrix(x5[,,2])
 x5[,,2][1,3]
-x5[1,3,2]
-x5[,,2][1:3]
-is.vector(x5[,,2][1:3])
-is.vector(x5[,,2][1:2])
-is.vector(x5[,,2][1])
+is.vector(x5[,,2][1,3])
