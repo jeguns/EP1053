903a9172f12ce32601ccb45d7796fa94a0ec9bca
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2021-03-24 16:06:30 -0500

    Actualización 24/03

--- Codigo 05.02 - Medidas de posicion.R
+++ Codigo 05.02 - Medidas de posicion.R
@@ -12,9 +12,11 @@ x = c(12,11,12,13,12,12,13,12,12,11,12,12)
 quantile(x, 0.25)
 sort(x)
 
+# Al menos el 25% de los alumnos tiene una edad menor o igual a 12 años
+
 # Aplicación VACUNAS
 
-Vacunas = read_csv("vacunas_covid_2021-03-16.csv")
+Vacunas = read_csv("vacunas_covid_2021-03-21.csv")
 
 Vacunas %>% 
   mutate(GRUPO_RIESGO     = GRUPO_RIESGO %>% as.factor,
@@ -41,7 +43,8 @@ VacunasOK %>%
   summarise(P35 = quantile(EDAD,0.35))
 
 VacunasOK %>% 
-  filter(!is.na(EDAD) & GRUPO_RIESGO == "ADULTO MAYOR" & DISTRITO == "MIRAFLORES") %>% 
+  filter(!is.na(EDAD) & GRUPO_RIESGO == "ADULTO MAYOR" & DISTRITO == "MIRAFLORES" & PROVINCIA=="LIMA") %>% 
   summarise(Q1 = quantile(EDAD,0.25),
             Q3 = quantile(EDAD,0.75))
+
 
