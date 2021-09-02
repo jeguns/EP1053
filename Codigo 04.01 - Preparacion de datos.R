b189f4cda4c821f0374d7f9fd304760241119cf2
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2021-09-02 09:38:53 -0500

    Actualizado 02/09

--- Codigo 04.01 - Preparacion de datos.R
+++ Codigo 04.01 - Preparacion de datos.R
@@ -109,12 +109,32 @@ datos41B |>
 datos41B
 datosOK
 
-# ---------------------- #
-# Manipulación de fechas #
-# ---------------------- #
+
+# Manipulación de fechas --------------------------------------------------
 
 Sys.Date()
 Sys.time()
+
+Sys.Date() |> str()
+Sys.time() |> str()
+
+Sys.Date() |> typeof()
+Sys.time() |> typeof()
+
+Sys.Date() |> as.numeric()
+Sys.time() |> as.numeric()
+
+as.Date("1970-01-01") |> as.numeric()
+
+as.Date("2021-09-02") - as.Date("2021-07-11")
+as.Date("2021-09-02") |> as.numeric()
+as.Date("2021-07-11") |> as.numeric()
+
+a = Sys.time()
+b = Sys.time()
+b-a
+
+as.Date("1980-01-01") + 366
 
 library(lubridate)
 today()
@@ -131,7 +151,10 @@ now(tz = "Etc/GMT+5")
 today(tz = "Australia/Eucla")
 now(tz = "Australia/Eucla")
 
+"2020-09-04"
 ymd("2020-09-04")
+"2020-09-04" |> typeof()
+ymd("2020-09-04") |> typeof()
 ymd("2020-Sep-04")
 dmy("04-Sep-2020")
 dmy("04-09-2020")
@@ -142,13 +165,14 @@ make_datetime(2020,09,04)
 make_datetime(day=04,month=09,year=2020)
 make_datetime(2020,09,04,11)
 make_datetime(2020,09,04,11,25)
-make_datetime(2020,09,04,11,25,40,tz="America/Lima") -> a
+make_datetime(2021,09,02,11,25,40,tz="America/Lima") -> a
 a
 year(a)
 month(a)
 mday(a) # día del mes
 yday(a) # día del año
 yday(today())
+today() |> yday()
 today() %>% yday()
 365-yday(today())
 wday(a) # día de la semana asumiendo que comienza en domingo
@@ -163,11 +187,11 @@ today() %>% wday(week_start = 1)
 quarter(today())
 today() %>% quarter()
 
+now() %>% am()
 now() %>% pm()
 
-# --------------------------------- #
-# Aplicación 3: Tarjetas de crédito #
-# --------------------------------- #
+
+# Aplicación 3: Tarjetas de crédito ---------------------------------------
 
 # Con esta aplicación veremos cómo realizar la transformación de variables
 # asimismo, repasaremos lo concerniente a selección de variables
@@ -177,20 +201,18 @@ datos41D = read.delim("Datos 04.01 - C.txt", encoding = "UTF-8")
 
 library(lubridate) # lubridate sirve para el manejo de fechas
 
-# las líneas 167 y 168 son equivalentes a la línea 165
-
 rename(datos41D, TIPO_TARJETA = 5)
+
 
 datos41D %>% 
   rename(TIPO_TARJETA = 5) %>% 
   mutate(NOMBRE_COMPLETO = paste(NOMBRES,APELLIDOS)) %>% 
-  mutate(MONTO_USD = MONTO_SOLES/3.64) %>% 
+  mutate(MONTO_USD = MONTO_SOLES/4.05) %>% 
   mutate(FECHA_PAGO1  = as.Date(FECHA_PAGO1,format=c("%d/%m/%Y"))) %>% 
   mutate(FECHA_COMPRA = as.Date(FECHA_COMPRA,format=c("%d/%m/%Y"))) %>% 
   mutate(TIEMPO = FECHA_PAGO1 - FECHA_COMPRA) %>% 
   mutate(MES    = month(FECHA_COMPRA)) %>% 
-  filter(MES == 1) %>% 
-  filter(TIEMPO > 30) %>% 
+  filter(MES == 1 & TIEMPO > 30) %>% 
   select(NOMBRE_COMPLETO, MONTO_USD, TIEMPO) -> MOROSOS
 
 MOROSOS
