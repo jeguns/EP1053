1e9f4db6ebefa1d54fb27608022f28e4e9a5a16e
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2021-12-10 12:25:26 -0500

    Actualizado 10/12

---
+++ Codigo 02.02 - Datos.R
@@ -0,0 +1,77 @@
+
+
+# Datos enteros -----------------------------------------------------------
+
+x = 4L
+typeof(x)
+is.integer(x)
+
+y = 4
+typeof(y)
+is.integer(y)
+
+# Datos flotantes ---------------------------------------------------------
+
+x = 36
+typeof(x)
+is.integer(x)
+is.double(x)
+
+y = 40.3
+typeof(y)
+is.integer(y)
+is.double(y)
+
+z = exp(4)
+typeof(z)
+is.integer(Z)
+is.double(Z)
+
+
+# Datos complejos ---------------------------------------------------------
+
+z = 1 + 2i
+typeof(z)
+is.integer(z)
+is.double(z)
+is.complex(z)
+
+t = 1i
+typeof(t)
+is.integer(t)
+is.double(t)
+is.complex(t)
+
+sqrt(-1)
+
+w = 10 + 0i
+typeof(w)
+is.integer(w)
+is.double(w)
+is.complex(w)
+
+v = 10
+typeof(v)
+is.integer(v)
+is.double(v)
+is.complex(v)
+
+
+# Datos lógicos -----------------------------------------------------------
+
+(a = FALSE)
+typeof(a)
+is.integer(a)
+is.double(a)
+is.complex(a)
+is.logical(a)
+
+3 == 5.2
+4.6 <= 8.1
+12 > 3.5
+5 != 8
+(w = 5 != 8)
+(w <- 5 != 8)
+(5 != 8 -> w)
+5 != 8 -> w
+typeof(w)
