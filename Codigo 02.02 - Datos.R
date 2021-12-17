e771cd6c5a40479bba508d4da96a23d4a24ae64c
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2021-12-17 11:38:12 -0500

    Actualizado 17/12

--- Codigo 02.02 - Datos.R
+++ Codigo 02.02 - Datos.R
@@ -102,8 +102,52 @@ r = 70096321
 is.character(r)
 is.double(r)
 
-R = as.integer(4L+40)
-typeof(R)
+# Coerción ----------------------------------------------------------------
+
+(A = TRUE + 2L)
+typeof(A)
+
+(B = 2L + 36.25)
+typeof(B)
+
+(C = '997658733')
+typeof(C)
+
+C = paste0("Tengo ",25L," años")
+typeof(C)
+
+D = 5L
+as.integer(D)
+as.complex(D)
+as.double(D)
+as.character(D)
 
 
+# Vectores atómicos -------------------------------------------------------
 
+x1 = c(3,4,5,12,3.4)
+typeof(x1)
+str(x1) # estructura 
+is.double(x1)
+is.atomic(x1)
+is.complex(x1)
+is.logical(x1)
+x1[1]
+x1[1:3]
+x1[c(1:3)]
+x1[c(1,2,3)]
+x1[-2]
+x1[-c(1:3)]
+
+a1 = c(11,11,11,11,11)
+is.atomic(a1)
+a2 = rep(11,5)
+a1
+a2
+a3 = c(1,2,3,5,6,6,6,6,6,7,10)
+a3 = c(1:3,5,rep(6,5),7,10)
+a3
+a3[c(1,5,8)]
+
+
+  
