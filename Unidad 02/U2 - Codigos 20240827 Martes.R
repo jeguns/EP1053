44cb0240d41c3ca1d8c23f0e207ce65f3eee54e2
jeguns <jesus.eduardo.gamboa@gmail.com>
2024-09-01 16:28:13 -0500

    added codes 27-08-24

---
+++ Unidad 02/U2 - Codigos 20240827 Martes.R
@@ -0,0 +1,62 @@
+
+
+# seccion1 ----------------------------------------------------------------
+
+2+6
+15-13
+5*5
+6/3
+36^0.5
+36**0.5
+36**(1/2)
+sqrt(36)
+73**0.25
+log10(1000) # logaritmo en base 10
+log2(64)  # logaritmo en base 2
+log(64,2) # logaritmo en base 2
+log(625,5)
+
+
+# seccion2 ----------------------------------------------------------------
+
+
+abs(-28)
+sin(0)
+cos(0)
+tan(0)
+sin(pi/2)
+factorial(3)
+choose(10,1)
+choose(6,3) # combinatoria de 6 en 3
+pi + 1
+round(3.548651, 2)
+ceiling(3.548651)
+floor(3.548651)
+?round
+?sqrt
+?choose
+exp(1)
+exp(5)
+x = exp(3) # A x se le asigna el valor exp(3)
+x <- exp(3)
+exp(3) -> x
+x == 2
+
+# seccion3 ----------------------------------------------------------------
+
+x1 <- sin(2*pi/3) + 1i
+x2 <- log(exp(1))+ 9
+x3 <- (sqrt(log10(10000)))**(1/89)
+x4 <- 1i+sin(3*pi/2)
+(x1 + x2)/x3 
+50%/%7
+50%%7
+
+
+# seccion4 ----------------------------------------------------------------
+
+z = 'hola'
+
+hola = 9
+sqrt(hola)
+
