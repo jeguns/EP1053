19a8f65bc932b87a0d076d6341c4f49f5efb9682
jeguns <jesus.eduardo.gamboa@gmail.com>
2025-09-01 18:33:29 -0500

    Create U2 - Codigos 20250826 Martes.R

---
+++ Unidad 02/U2 - Codigos 20250826 Martes.R
@@ -0,0 +1,92 @@
+m <- 3*5
+a <- log10(1000) - abs(-4)
+f <- cos(pi/2) + exp(4)
+h <- sin(pi/6) + sqrt(25) - 5**(1/3)
+a
+y <- sqrt(sqrt(sqrt(64)))
+y
+
+x = 4L
+typeof(x)
+is.numeric(x)
+
+y = 54.25
+is.numeric(y)
+
+z = 1 + 2i
+is.numeric(z)
+
+v = 10
+typeof(v)
+is.integer(v)
+is.double(v)
+is.complex(v)
+
+3 == 5.2
+4.6 <= 8.1
+12 > 3.5
+5 != 8
+b = 4 == 5
+b
+
+w = T
+w
+r = F
+r
+
+p = 'Universidad Agraria'
+typeof(p)
+
+q = "Universidad Agraria"
+typeof(q)
+
+s = a
+s = "d"
+
+u = "FALSE"
+is.logical(u)
+
+k = '5L'
+is.integer(k)
+
+o = "'perro'"
+o
+typeof(o)
+
+h = floor(3.6)
+typeof(h)
+
+u = ceiling(3.6)
+u
+
+round(3.6)
+round(3.2)
+
+a <- log(70,4)-abs(81**(1/3))
+a
+typeof(a)
+
+r = TRUE + 2L
+typeof(r)
+e = TRUE + 2
+typeof(e)
+
+w = 2**TRUE + 2
+typeof(w)
+w
+
+y = TRUE + TRUE + 0L
+y
+typeof(y)
+
+
+5 + '5'
+
+
+c(2.5, p, 'R')
+
+as.character(32231)
+as.double(TRUE)
+
+as.double('115')
+as.double('abc')
